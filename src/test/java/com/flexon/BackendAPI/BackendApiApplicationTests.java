83d1a1b0b63ce31d319bd2bfd4a53c69576c6885
jerome <jeromezhang1996@gmail.com>
2021-03-10 20:27:11 -0800

    added tests

--- src/test/java/com/flexon/BackendAPI/BackendApiApplicationTests.java
+++ src/test/java/com/flexon/BackendAPI/BackendApiApplicationTests.java
@@ -1,13 +1,54 @@
 package com.flexon.BackendAPI;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.Connection;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.flexon.BackendAPI.services.CatalogDAO;
+import com.flexon.BackendAPI.services.OrderDAO;
+import com.flexon.BackendAPI.utils.ConnSingleton;
+import com.flexon.BackendAPI.models.Item;
+import com.flexon.BackendAPI.models.Order;
 
 @SpringBootTest
 class BackendApiApplicationTests {
 
+	ConnSingleton cu = ConnSingleton.getConnSingleton();
+	Connection conn = cu.getConnection();
+	OrderDAO orderDAO = new OrderDAO();
+	CatalogDAO catalogDAO = new CatalogDAO();
+
+	
 	@Test
-	void contextLoads() {
+	public void testGetOrderIDReturnsOrderType() {
+		Order testOrder = orderDAO.getOrderID(2);		
+		assertEquals(testOrder instanceof Order, true);
 	}
+
+	@Test
+	public void testAcceptShipDate() {
+		Order testOrder = orderDAO.getOrderID(2);
+		assertEquals(testOrder.isAccepted(),false);
+		assertEquals(orderDAO.acceptShipDate(2), true);		
+	}
+
+	@Test
+	public void testGetSKU() {
+		Item testItem = catalogDAO.getSKU(99999998);
+		assertEquals(testItem instanceof Item, true);
+	}
+	
+	@Test
+	public void testCancelItem() {
+		assertEquals(orderDAO.cancelItem(2, 3), true);
+	}
+
+	@Test
+	public void testCancelOrder() {
+		assertEquals(orderDAO.cancelOrder(2),true);
+	}
+
 
 }
