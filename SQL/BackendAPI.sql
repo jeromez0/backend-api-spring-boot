31d786d57a91ac7c99980e306c5290715c158751
jerome <jeromezhang1996@gmail.com>
2021-03-10 20:30:39 -0800

    added updated SQL script

--- SQL/BackendAPI.sql
+++ SQL/BackendAPI.sql
@@ -1,3 +1,15 @@
+/*******************************************************************************
+   BankApplication Database
+   Script: BackEndAPI.sql
+   Description: Creates and populates the relevant database.
+   DB Server: MySQL
+   Author: Jerome Zhang
+********************************************************************************/
+
+/*
+** Create Tables
+*/
+
 drop table if exists Items;
 drop table if exists Orders;
 
@@ -16,15 +28,22 @@ create table Items(
  SKU numeric(9) unique not null,
  Quantity int,
  orderID int,
- catalog boolean default False,
- foreign key (orderID)
-	references Orders (orderID)
+ catalog boolean default False
  );
 
+/*
+** Populate Tables
+*/
 insert into Orders(orderID, Accepted, Deleted) values (1, false, false);
 insert into Items (item_name, Price, SKU, Quantity, orderID) values ("fishing rod", 29.99, 123456789, 3, 1);
 insert into Items (item_name, Price, SKU, Quantity, orderID) values ("playstation", 299.99, 11111111, 1, 1);
 
-select * from Orders;
+-- testing 
+insert into Orders(orderID, Accepted, Deleted) values (2, false, false);
+insert into Items (item_name, Price, SKU, Quantity, orderID) values ("object1", 29.99, 999999999, 3, 2);
+insert into Items (item_name, Price, SKU, Quantity, orderID) values ("object2", 299.99, 99999998, 1, 2);
 
-select * from bankDB.Items where orderID = 1;
+
+select * from Orders;
+-- delete from Orders where orderID=2;
+-- select * from bankDB.Items where orderID = 1;
